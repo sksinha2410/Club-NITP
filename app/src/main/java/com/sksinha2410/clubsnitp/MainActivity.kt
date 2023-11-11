4fa908ab37768640022adeaee11fb08d3d2688e8
Shivam Kumar Sinha <sksinha200276@gmail.com>
2023-11-11 16:08:30 +0530

    fixed bugs

--- app/src/main/java/com/sksinha2410/clubsnitp/MainActivity.kt
+++ app/src/main/java/com/sksinha2410/clubsnitp/MainActivity.kt
@@ -21,44 +21,44 @@ class MainActivity : AppCompatActivity() {
         }
 
         val ieee = findViewById<Button>(R.id.button2)
-        desco.setOnClickListener {
+        ieee.setOnClickListener {
             val Intent = Intent(this,IEEE::class.java)
             startActivity(Intent)
         }
 
         val tesla = findViewById<Button>(R.id.button3)
-        desco.setOnClickListener {
+        tesla.setOnClickListener {
             val Intent = Intent(this,TESLA::class.java)
             startActivity(Intent)
         }
 
         val hackslash = findViewById<Button>(R.id.button4)
-        desco.setOnClickListener {
+        hackslash.setOnClickListener {
             val Intent = Intent(this,HAckSlash::class.java)
             startActivity(Intent)
         }
 
 
         val asme = findViewById<Button>(R.id.button5)
-        desco.setOnClickListener {
+        asme.setOnClickListener {
             val Intent = Intent(this,ASME::class.java)
             startActivity(Intent)
         }
 
         val gdsc = findViewById<Button>(R.id.button6)
-        desco.setOnClickListener {
+        gdsc.setOnClickListener {
             val Intent = Intent(this,GDSC::class.java)
             startActivity(Intent)
         }
 
         val saptak = findViewById<Button>(R.id.button7)
-        desco.setOnClickListener {
+        saptak.setOnClickListener {
             val Intent = Intent(this,SAPTAK::class.java)
             startActivity(Intent)
         }
 
         val expresso = findViewById<Button>(R.id.button8)
-        desco.setOnClickListener {
+        expresso.setOnClickListener {
             val Intent = Intent(this,EXPRESSO::class.java)
             startActivity(Intent)
         }
