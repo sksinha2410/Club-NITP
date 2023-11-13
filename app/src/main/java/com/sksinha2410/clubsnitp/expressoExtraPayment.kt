5530865f792bd424f23e6c30eda8d96a63ab11da
Shivam Kumar Sinha <sksinha200276@gmail.com>
2023-11-13 20:02:26 +0530

    Added extra payment pages for all clubs

---
+++ app/src/main/java/com/sksinha2410/clubsnitp/expressoExtraPayment.kt
@@ -0,0 +1,11 @@
+package com.sksinha2410.clubsnitp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class expressoExtraPayment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expresso_extra_payment)
+    }
+}
---
+++ app/src/main/java/com/sksinha2410/clubsnitp/ieeeExtraPayment.kt
@@ -0,0 +1,11 @@
+package com.sksinha2410.clubsnitp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ieeeExtraPayment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ieee_extra_payment)
+    }
+}
