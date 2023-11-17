7908ed73da55e927ca5edb1801cb9cd494f3b722
Shivam Kumar Sinha <sksinha200276@gmail.com>
2023-11-17 12:05:29 +0530

    created payment pages for newly added clubs

---
+++ app/src/main/java/com/sksinha2410/clubsnitp/natvanshPaymentPage.kt
@@ -0,0 +1,11 @@
+package com.sksinha2410.clubsnitp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class natvanshPaymentPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_natvansh_payment_page)
+    }
+}
