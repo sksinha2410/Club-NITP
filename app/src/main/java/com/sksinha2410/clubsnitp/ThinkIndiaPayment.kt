de3263acaa60f6b0d942295b7ea31a6bcdc2f30e
Shivam Kumar Sinha <sksinha200276@gmail.com>
2023-11-22 15:27:41 +0530

    added payment gateway to all clubs

--- app/src/main/java/com/sksinha2410/clubsnitp/ThinkIndiaPayment.kt
+++ app/src/main/java/com/sksinha2410/clubsnitp/ThinkIndiaPayment.kt
@@ -3,7 +3,10 @@ package com.sksinha2410.clubsnitp
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class ThinkIndiaPayment : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -11,7 +14,10 @@ class ThinkIndiaPayment : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_think_india_payment)
 
-        val webView = findViewById<WebView>(R.id.webview)
-        webView.loadUrl("https://pages.razorpay.com/vimantriki/")
+        var mywebView = findViewById<View>(R.id.webview) as WebView
+        mywebView.setWebViewClient(WebViewClient())
+        mywebView.loadUrl("https://pages.razorpay.com/pl_N3Tew8H6PY2Sfp/view/")
+        val webSettings: WebSettings = mywebView.getSettings()
+        webSettings.javaScriptEnabled = true
     }
 }
--- app/src/main/java/com/sksinha2410/clubsnitp/descoPayment.kt
+++ app/src/main/java/com/sksinha2410/clubsnitp/descoPayment.kt
@@ -17,7 +17,7 @@ class descoPayment : AppCompatActivity() {
 
         var mywebView = findViewById<View>(R.id.webview) as WebView
         mywebView.setWebViewClient(WebViewClient())
-        mywebView.loadUrl("https://pages.razorpay.com/vimantriki/")
+        mywebView.loadUrl("https://pages.razorpay.com/pl_N3Tew8H6PY2Sfp/view/")
         val webSettings: WebSettings = mywebView.getSettings()
         webSettings.javaScriptEnabled = true
     }
